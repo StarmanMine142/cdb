f5e86cf7e81736b5a9666894692bb87352f8ce46
Starman <135715391+StarmanMine142@users.noreply.github.com>
2026-07-11 00:22:16 +0300

    improvements

--- src/main/java/com/starman/cdb/CannonDestroyingBlock.java
+++ src/main/java/com/starman/cdb/CannonDestroyingBlock.java
@@ -1,10 +1,7 @@
 package com.starman.cdb;
 
 import com.simibubi.create.api.registry.CreateRegistries;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
