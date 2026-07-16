0bcaf69fae739aae1e074492593dcad2d5639538
Starman <135715391+StarmanMine142@users.noreply.github.com>
2026-07-16 23:35:26 +0300

    1.0.2: specific block id

--- src/main/java/com/starman/cdb/CannonPreset.java
+++ src/main/java/com/starman/cdb/CannonPreset.java
@@ -2,14 +2,16 @@ package com.starman.cdb;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+import java.util.Optional;
 
-public record CannonPreset(int breakSteps, TagKey<Block> tag) {
+public record CannonPreset(int breakSteps, Optional<TagKey<Block>> tag, Optional<Block> block) {
     public static final Codec<CannonPreset> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.INT.fieldOf("break_steps").forGetter(CannonPreset::breakSteps),
-            ResourceLocation.CODEC.fieldOf("tag").xmap(id -> TagKey.create(Registries.BLOCK, id), TagKey::location).forGetter(CannonPreset::tag)
+            TagKey.codec(Registries.BLOCK).optionalFieldOf("tag").forGetter(CannonPreset::tag),
+            BuiltInRegistries.BLOCK.byNameCodec().optionalFieldOf("block").forGetter(CannonPreset::block)
     ).apply(instance, CannonPreset::new));
 }
