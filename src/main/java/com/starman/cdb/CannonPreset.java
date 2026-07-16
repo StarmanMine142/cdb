5e76ff61d49c8303cb471116563b8d46f32edf65
Starman <135715391+StarmanMine142@users.noreply.github.com>
2026-07-17 00:39:14 +0300

    fix again

--- src/main/java/com/starman/cdb/CannonPreset.java
+++ src/main/java/com/starman/cdb/CannonPreset.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public record CannonPreset(int breakSteps, Optional<TagKey<Block>> tag, Optional<Block> block) {
     public static final Codec<CannonPreset> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            Codec.INT.fieldOf("break_steps").forGetter(CannonPreset::breakSteps),
+            Codec.intRange(1, 10).optionalFieldOf("break_steps", 1).forGetter(CannonPreset::breakSteps),
             TagKey.codec(Registries.BLOCK).optionalFieldOf("tag").forGetter(CannonPreset::tag),
             BuiltInRegistries.BLOCK.byNameCodec().optionalFieldOf("block").forGetter(CannonPreset::block)
     ).apply(instance, CannonPreset::new));
